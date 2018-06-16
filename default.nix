403cf31301faf6615d6a45d027e9ed4746539e66
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-06-16 11:50:38 +0200

    Make use of nix-2.0 fetchGit

---
+++ default.nix
@@ -0,0 +1,32 @@
+# To get a development shell use:
+#   nix-shell
+# To build with pinned nixpkgs use:
+#   nix build
+# To develop/build with your custom nixpkgs (from $NIX_PATH) use:
+#   nix-shell --arg nixpkgs "<nixpkgs>"
+#   nix build --arg nixpkgs "<nixpkgs>"
+
+{ nixpkgs ? builtins.fetchGit {
+    url = git://github.com/NixOS/nixpkgs-channels;
+    ref = "nixos-18.03";
+    rev = "08d245eb31a3de0ad73719372190ce84c1bf3aee";
+  }
+}:
+let
+  pkgs = import nixpkgs { config = { }; overlays = [ ]; };
+in with pkgs; stdenv.mkDerivation rec {
+  name = "hydra-ci-example-cpp";
+
+  src = if lib.inNixShell then null else ./.;
+
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  doCheck = true;
+  checkTarget = "test";
+
+  meta.maintainers = [
+    "Josef Kemetmueller <josef.kemetmueller@gmail.com>"
+  ];
+}
--- release.nix
+++ release.nix
@@ -1,13 +1,3 @@
 rec {
-  build = with import <nixpkgs> { };
-  releaseTools.nixBuild {
-    name = "helloworld";
-    src = ./.;
-    doCheck = true;
-    checkTarget = "test";
-    buildInputs = [ cmake ];
-    meta.maintainers = [ 
-      "Josef Kemetmueller <josef.kemetmueller@gmail.com>"
-    ]; 
-  };
+  build = import ./default.nix { };
 }
