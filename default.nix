b16bbc3b7ea973cf33a2dc5d0b3862222b675401
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-06-16 12:14:38 +0200

    Build only what's commited

--- default.nix
+++ default.nix
@@ -6,18 +6,19 @@
 #   nix-shell --arg nixpkgs "<nixpkgs>"
 #   nix build --arg nixpkgs "<nixpkgs>"
 
-{ nixpkgs ? builtins.fetchGit {
+{ nixpkgs ? (builtins.fetchGit {
     url = git://github.com/NixOS/nixpkgs-channels;
     ref = "nixos-18.03";
     rev = "08d245eb31a3de0ad73719372190ce84c1bf3aee";
-  }
+  })
+, src ? builtins.fetchGit ./.
 }:
 let
   pkgs = import nixpkgs { config = { }; overlays = [ ]; };
 in with pkgs; stdenv.mkDerivation rec {
   name = "hydra-ci-example-cpp";
 
-  src = if lib.inNixShell then null else ./.;
+  inherit src;
 
   nativeBuildInputs = [
     cmake
